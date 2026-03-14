be9b2ffa2e5e3e4be50c55248dbafcfa27c93f91
Giovanny <gikaorsini@gmail.com>
2026-03-14 13:29:19 -0300

    Added a list of students and a JSON generator to exercicio1.py program

--- Modelagem-Banco-de-Dados/exercicio1.py
+++ Modelagem-Banco-de-Dados/exercicio1.py
@@ -5,8 +5,11 @@ import json
 # Criando um dicionário com dados de um aluno
 aluno = {
     "nome": "Carlos",
+    "sobrenome": "Santana",
     "idade": 21,
-    "curso": "ADS"
+    "curso": "ADS",
+    "ano de inicio": 2025,
+    "previsao de conclusao": 2027
 }
 
 # Salvando os dados em um arquivo JSON
@@ -30,7 +33,21 @@ with open("aluno.json", "r") as arquivo:
 print("Nome:", dados["nome"])
 print("Idade:", dados["idade"])
 print("Curso:", dados["curso"])
+print("Ano de inicio:", dados["ano de inicio"])
+print("Previsao de conclusao:", dados["previsao de conclusao"])
 
 # Lista de alunos em JSON
 
 import json
+
+# Lista de alunos
+alunos = [
+    {"nome": "Ana", "nota": 8.5},
+    {"nome": "Pedro", "nota": 7.0},
+    {"nome": "Maria", "nota": 9.2}
+]
+
+# Salvando no arquivo JSON
+
+with open("alunos.json", "w") as arquivo:
+    json.dump(alunos, arquivo)
