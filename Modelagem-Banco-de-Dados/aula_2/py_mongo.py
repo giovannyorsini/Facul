6f8ee463f3f06fc29bc191b9f43e1f38189f7b91
Giovanny <gikaorsini@gmail.com>
2026-04-11 14:03:10 -0300

    Updated folder structure and added a new execise in aula_2 folder

---
+++ Modelagem-Banco-de-Dados/aula_2/py_mongo.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["escola"]
+
+colecao = db["alunos"]
+
+# inserir
+# colecao.insert_one({"nome": "Bruno", "idade": 29})
+# colecao.insert_many([
+#     { "nome": "Leila", "idade": 27},
+#     { "nome": "Rodrigo Alves", "idade": 38 },
+#     { "nome": "Simone Rocha", "idade": 34 },
+#     { "nome": "Gustavo Henrique", "idade": 29 },
+#     { "nome": "Julio Cesar", "idade": 41 }
+# ])
+
+# buscar
+for aluno in colecao.find():
+    print(aluno)
+
+# atualizar
+# colecao.update_one(
+#     { "nome": "Bruno" },
+#     { "$set": { "idade": 30 } }
+# )
+
+colecao.update_many(
+    { "idade": 27},
+    { "$set": { "idade": 20 }},
+)
